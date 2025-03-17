555a5d512fdf92c3233bcd6eea9504f5482b7118
Emil J. Tywoniak <emil@tywoniak.eu>
2025-03-17 12:39:55 +0100

    Initial commit (or is it?)

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Generic devshell setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+  };
+  outputs = { self, nixpkgs, flake-compat, flake-utils }@inputs:
+    let
+      overlay = import ./nix/overlay.nix;
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { overlays = [ overlay ]; inherit system; };
+        in
+        {
+          # Help other use packages in this flake
+          legacyPackages = pkgs;
+
+          devShells.default = pkgs.buddy-llvm.devShell;
+
+          formatter = pkgs.nixpkgs-fmt;
+        }) //
+    {
+      # Other system-independent attr
+      inherit inputs;
+      flake-compat = inputs.flake-compat;
+      overlays.default = overlay;
+    };
+}
