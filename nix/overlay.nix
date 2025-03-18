b4d1da2a9b8afb78743ba48c54944503e630b7fb
Emil J. Tywoniak <emil@tywoniak.eu>
2025-03-18 13:41:35 +0100

    Build smaller yosys with lld

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,6 +4,18 @@ final: prev:
   llvmPkgs = final.llvmPackages_17;
   # Use clang instead of gcc to compile, to avoid gcc13 miscompile issue.
   rtlil-llvm = final.callPackage ./rtlil-llvm.nix { stdenv = final.llvmPkgs.stdenv; };
-  clang-yosys = (final.pkgs.yosys.override { stdenv = final.llvmPkgs.stdenv; enablePython = false; gtkwave = null; }).overrideAttrs { doCheck = false; };
+  clang-yosys = (final.pkgs.yosys.override {
+    stdenv = final.llvmPkgs.stdenv;
+    enablePython = false;
+    gtkwave = null;
+  }).overrideAttrs (finalAttrs: previousAttrs: {
+    doCheck = false;
+    nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ final.llvmPackages.lld ];
+    makeFlags = previousAttrs.makeFlags ++ [
+      "LINKFLAGS=-fuse-ld=lld"
+      "SMALL=1"
+      "ENABLE_ABC=0"
+    ];
+  });
   # rtlil-mlir = final.callPackage ./rtlil-mlir.nix { stdenv = final.llvmPkgs.stdenv; };
 }
