db4e51c67c2f913fdb17bc3156caf1cb98479364
Emil J. Tywoniak <emil@tywoniak.eu>
2025-04-11 16:20:38 +0200

    Start frontend

--- rtlil-emit/rtlil-emit.cpp
+++ rtlil-emit/rtlil-emit.cpp
@@ -8,9 +8,11 @@
 #include "mlir/Parser/Parser.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Support/FileUtilities.h"
+#include "mlir/Support/LogicalResult.h"
 #include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
 #include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
 #include "mlir/Target/LLVMIR/Export.h"
+#include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_os_ostream.h"
 
 #include "RTLIL/RTLILDialect.h"
@@ -114,7 +116,8 @@ public:
   }
 
   rtlil::WConnectionOp convert_connection(RTLIL::SigSig ss) {
-    log_debug("converting connection %s %s\n", log_signal(ss.first), log_signal(ss.second));
+    log_debug("converting connection %s %s\n", log_signal(ss.first),
+              log_signal(ss.second));
     return b.create<rtlil::WConnectionOp>(loc, convert_sigspec(ss.first),
                                           convert_sigspec(ss.second));
   }
@@ -144,13 +147,100 @@ struct MlirBackend : public Backend {
     log_header(design, "Executing MLIR backend.\n");
     size_t argidx;
     for (argidx = 1; argidx < args.size(); argidx++) {
+      break;
     }
     extra_args(f, filename, args, argidx);
     llvm::raw_os_ostream osos(*f);
     mlir::MLIRContext ctx;
     ctx.getOrLoadDialect<rtlil::RTLILDialect>();
-    MLIRifier mlirifier(ctx);
+    MLIRifier convertor(ctx);
     for (auto mod : design->selected_modules())
-      mlirifier.convert_module(mod).print(osos);
+      convertor.convert_module(mod).print(osos);
   }
-} MyPass;
+} MlirBackend;
+
+class RTLILifier {
+  RTLIL::Design *design;
+
+public:
+  RTLILifier(RTLIL::Design* d) : design(d) {}
+  void convert_wire(RTLIL::Module* mod, rtlil::WireOp op) {
+    auto name = op->getAttr("name").cast<mlir::StringAttr>().str();
+    // mlir::IntegerType::get(&ctx, 32),
+    RTLIL::Wire* w = mod->addWire(name);
+    w->width = op->getAttr("width").cast<mlir::IntegerAttr>().getInt();
+    w->start_offset = op->getAttr("start_offset").cast<mlir::IntegerAttr>().getInt();
+    w->port_id = op->getAttr("port_id").cast<mlir::IntegerAttr>().getInt();
+    w->port_input = op->getAttr("port_input").cast<mlir::BoolAttr>().getValue();
+    w->port_output = op->getAttr("port_output").cast<mlir::BoolAttr>().getValue();
+    w->upto = op->getAttr("upto").cast<mlir::BoolAttr>().getValue();
+    w->is_signed = op->getAttr("is_signed").cast<mlir::BoolAttr>().getValue();
+  }
+  void convert_cell(RTLIL::Module* mod, rtlil::CellOp op) {
+  }
+  void convert_connection(RTLIL::Module* mod, rtlil::WConnectionOp op) {
+  }
+  void convert_const(RTLIL::Module* mod, rtlil::ConstOp op) {
+  }
+  void convert_module(mlir::ModuleOp moduleOp) {
+    llvm::StringRef moduleName = moduleOp.getName().value_or("");
+    log_assert((moduleName.size() != 0) && "Unnamed module op in RTLIL dialect");
+    RTLIL::Module* new_module = design->addModule(moduleName.str());
+    for (auto &op : moduleOp.getBody()->getOperations()) {
+      if (auto wireOp = mlir::dyn_cast<rtlil::WireOp>(op))
+        convert_wire(new_module, wireOp);
+      else if (auto cellOp = mlir::dyn_cast<rtlil::CellOp>(op))
+        convert_cell(new_module, cellOp);
+      else if (auto connOp = mlir::dyn_cast<rtlil::WConnectionOp>(op))
+        convert_connection(new_module, connOp);
+      else if (auto constOp = mlir::dyn_cast<rtlil::ConstOp>(op))
+        convert_const(new_module, constOp);
+      else {
+        op.dump();
+        log_error("Unhandled RTLIL dialect op\n");
+      }
+    }
+  }
+};
+
+struct MlirFrontend : public Frontend {
+  MlirFrontend() : Frontend("mlir", "Read design from MLIR RTLIL dialect") {}
+  // TODO help
+  void execute(std::istream *&f, std::string filename,
+               std::vector<std::string> args, RTLIL::Design *design) override {
+    log_header(design, "Executing MLIR frontend.\n");
+    size_t argidx;
+    for (argidx = 1; argidx < args.size(); argidx++) {
+      break;
+    }
+    extra_args(f, filename, args, argidx);
+    //   llvm::raw_istream osis(*f);
+    mlir::MLIRContext ctx;
+    ctx.getOrLoadDialect<rtlil::RTLILDialect>();
+    llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> fileOrErr =
+        llvm::MemoryBuffer::getFileOrSTDIN(filename);
+    if (std::error_code ec = fileOrErr.getError()) {
+      llvm::errs() << "Could not open input file: " << ec.message() << "\n";
+    }
+
+    // Parse the input mlir.
+    llvm::SourceMgr sourceMgr;
+    sourceMgr.AddNewSourceBuffer(std::move(*fileOrErr), llvm::SMLoc());
+    mlir::OwningOpRef<mlir::ModuleOp> owningModule =
+        mlir::parseSourceFile<mlir::ModuleOp>(sourceMgr, &ctx);
+    if (!owningModule) {
+      llvm::errs() << "Error can't load file " << filename << "\n";
+    }
+    auto moduleOp = std::make_shared<mlir::ModuleOp>(owningModule.release());
+    llvm::outs() << "yeah we got some stuff\n";
+    // moduleOp->print(llvm::outs());
+    // auto opIterator = .begin();
+    RTLILifier convertor(design);
+    for (auto& operation : moduleOp->getOps()) {
+      mlir::ModuleOp op = llvm::dyn_cast<mlir::ModuleOp>(operation);
+      if (!op)
+        log_assert(false && "Top level MLIR entity isn't a module");
+      convertor.convert_module(op);
+    }
+  }
+} MlirFrontend;
